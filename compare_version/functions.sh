09af81ee0f19f6091e8764e56819fcd9e0c87be9
Kai Chen <kai@sorrentocorp.com>
2017-11-04 19:41:26 -0700

    feat: can compare versions such as 1.5rc1 with 1.5 correctly

--- compare_version/functions.sh
+++ compare_version/functions.sh
@@ -17,10 +17,9 @@ function ver_cmp () {
     echo 0
     return
   fi
-  local a=$(sanitize_version $1) b=$(sanitize_version $2)
   # split $1 by '.' into ver1 and $2 by '.' into ver2
   local IFS=.
-  local i ver1=($a) ver2=($b) # same as read -r -a ver1 <<<"$1", etc
+  local i ver1=($1) ver2=($2) # same as read -r -a ver1 <<<"$1", etc
   # fill empty fields in ver1 with zeros
   for ((i=${#ver1[@]}; i<${#ver2[@]}; i++)); do
     ver1[i]=0
@@ -42,15 +41,49 @@ function ver_cmp () {
 
 # compares version-wise two the parameters (a,b)
 # where a and b are single version fields, i.e. without dots
-# and contain only digits, which may be 0-padded
+# but may contain alphanumeric letters
 #
 # echoes -1 if a < b, 0 if a == b, and 1 if a > b
 function ver_cmp0() {
-  if (( 10#$1 < 10#$2 )); then
+  local a=$1 b=$2 a2=0 b2=0
+  local r1="([0-9]+)[-+a-zA-Z]+$"    # of the form 2arb_text+stuff
+  local r2="([0-9]+)[-+a-zA-Z]+([0-9]+)$" # of the form 2rc1
+
+  if [[ $a =~ $r1 ]]; then
+    a=${BASH_REMATCH[1]}
+  fi
+  if [[ $a =~ $r2 ]]; then
+    a=${BASH_REMATCH[1]}
+    a2=${BASH_REMATCH[2]}
+  fi
+
+  if [[ $b =~ $r1 ]]; then
+    b=${BASH_REMATCH[1]}
+  fi
+  if [[ $b =~ $r2 ]]; then
+    b=${BASH_REMATCH[1]}
+    b2=${BASH_REMATCH[2]}
+  fi
+
+  if (( 10#$a < 10#$b )); then
     echo -1
-  elif (( 10#$1 > 10#$2 )); then
+  elif (( 10#$a > 10#$b )); then
     echo 1
   else
-    echo 0
+    if (( a2 == 0 && b2 == 0 )); then
+      echo 0
+    elif (( a2 == 0 && b2 != 0)); then
+      echo 1  # e.g. 1.2 and 1.2+hot should come after 1.2rc1
+    elif (( a2 != 0 && b2 == 0 )); then
+      echo -1
+    else
+      if (( 10#$a2 < 10#$b2 )); then
+        echo -1
+      elif (( 10#$a2 > 10#$b2 )); then
+        echo 1
+      else
+        echo 0
+      fi
+    fi
   fi
 }
